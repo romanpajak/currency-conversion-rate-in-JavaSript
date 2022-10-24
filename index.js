f403f990c9ba2cb7b0c9b02fe190667be40f4f0a
romanpajak <romanpajak@interia.pl>
2022-10-24 16:25:46 +0200

    project finished

---
+++ index.js
@@ -0,0 +1,38 @@
+const alertBox = document.getElementById("equal");
+const currencyBox = document.getElementById("sum");
+function showAlert(text) {
+  document.getElementById("sum").innerHTML = "";
+  alertBox.innerHTML = text;
+  alertBox.style.color = "red";
+  currencyBox.style.display = "none";
+}
+function getCurrencyVal() {
+  alertBox.style.color = "black";
+  currencyBox.style.display = "block";
+  const currencyKind = document.getElementById("currencyKind").value;
+  const currencyAmount = document.getElementById("currAmount").value;
+  if (currencyAmount == "" || currencyAmount <= 0) {
+    showAlert("wpisz wartość liczbową powyżej zera");
+    document.getElementById("calculator").reset();
+    return;
+  }
+  const mainBtn = document.querySelector("#btn-calc");
+  mainBtn.style.color = "white";
+  mainBtn.classList.add("button-loading");
+  const url = `https://api.nbp.pl/api/exchangerates/rates/c/${currencyKind.toLocaleLowerCase()}/today/?format=json`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const currencyBid = data.rates[0].bid;
+      document.getElementById("sum").innerHTML = (
+        Math.round(currencyBid * currencyAmount * 100) / 100
+      ).toFixed(2);
+      document.getElementById("calculator").reset();
+      document.querySelector(
+        "#equal"
+      ).textContent = `${currencyAmount} ${currencyKind.toLocaleUpperCase()} to`;
+      mainBtn.classList.remove("button-loading");
+      mainBtn.style.color = "black";
+    })
+    .catch((err) => showAlert(err));
+}
