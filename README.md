d64b96098691fb7bac6172ab52d1c69c18338f4c
romanpajak <romanpajak@interia.pl>
2022-11-04 13:23:13 +0100

    project corrected

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Project-JS-2
+It's a short project, that calculates 3 foreign currencies (USD,EUR,CHF) for polish PLN. It uses the technologies like Bootstrap and Fetch API. 
--- index.js
+++ index.js
@@ -1,24 +1,34 @@
 const alertBox = document.getElementById("equal");
 const currencyBox = document.getElementById("sum");
+const mainBtn = document.querySelector("#btn-calc");
 function showAlert(text) {
   document.getElementById("sum").innerHTML = "";
   alertBox.innerHTML = text;
   alertBox.style.color = "red";
   currencyBox.style.display = "none";
 }
-function getCurrencyVal() {
+function restoreFormToDefaultStyle() {
   alertBox.style.color = "black";
   currencyBox.style.display = "block";
+}
+function loadSpinner() {
+  mainBtn.style.color = "white";
+  mainBtn.classList.add("button-loading");
+}
+function unloadSpinner() {
+  mainBtn.classList.remove("button-loading");
+  mainBtn.style.color = "black";
+}
+function getCurrencyVal() {
+  restoreFormToDefaultStyle();
   const currencyKind = document.getElementById("currencyKind").value;
   const currencyAmount = document.getElementById("currAmount").value;
-  if (currencyAmount == "" || currencyAmount <= 0) {
+  if (currencyAmount === "" || currencyAmount <= 0) {
     showAlert("wpisz wartość liczbową powyżej zera");
     document.getElementById("calculator").reset();
     return;
   }
-  const mainBtn = document.querySelector("#btn-calc");
-  mainBtn.style.color = "white";
-  mainBtn.classList.add("button-loading");
+  loadSpinner();
   const url = `https://api.nbp.pl/api/exchangerates/rates/c/${currencyKind.toLocaleLowerCase()}/today/?format=json`;
   fetch(url)
     .then((response) => response.json())
@@ -31,8 +41,7 @@ function getCurrencyVal() {
       document.querySelector(
         "#equal"
       ).textContent = `${currencyAmount} ${currencyKind.toLocaleUpperCase()} to`;
-      mainBtn.classList.remove("button-loading");
-      mainBtn.style.color = "black";
+      unloadSpinner();
     })
     .catch((err) => showAlert(err));
 }
